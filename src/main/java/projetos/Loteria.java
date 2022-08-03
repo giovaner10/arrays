99bd16dc7dc264bb00f946b39e8dfd0b1d989bdd
giovaner10 <giovanerr10@gmail.com>
2022-08-03 11:27:20 -0300

    initial commit

--- src/main/java/projetos/Loteria.java
+++ src/main/java/projetos/Loteria.java
@@ -1,14 +1,26 @@
 package projetos;
 
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Loteria {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner ler = new Scanner(System.in);
 
         int[] sorteados = new int[7];
 
+        ArrayList<String> resultados = new ArrayList<>();
+
         int bubbleAux;
+
+        int quantidadeDeJogos = 0;
+
+
+        int acertoDeNumeros = 0;
+        int acertoDeEstrelas = 0;
 
 
         for (int i = 0; i < 5; i++) {
@@ -64,6 +76,83 @@ public class Loteria {
             if(i > 4){
 
                 System.out.println("ESTRELAS sorteados["+i+"] = " + sorteados[i]);
+            }
+
+        }
+
+
+        while (true){
+
+            System.out.println("Bem vindo a loteria, aqui voce faz suas apostas!");
+            System.out.println("Carregando...");
+            Thread.sleep(2000);
+
+
+            System.out.println("Para continuar pressione qualquer tecla, ou digite 100 para finalizar");
+
+            int continuar = ler.nextInt();
+
+            if( continuar == 100){
+
+                resultados.forEach(resultado -> System.out.println(resultado));
+
+                System.out.println("Encerrando aposta, obrigado pela preferencia");
+                break;
+            }
+
+
+
+            for (int i = 0; i < 7; i++) {
+
+
+                if(i < 5){
+                    System.out.println("Informe o numero da " + (i+1) +"° posicao");
+                    int numero = ler.nextInt();
+
+                    while (numero < 1 || numero > 50 ){
+                        System.out.println("Valor invalido, informe dezenas entre 1 e 50. Digite novamente.");
+                        System.out.println("Informe o numero da " + (i+1) +"° posicao");
+
+                        numero = ler.nextInt();
+                    }
+
+                    if(numero == sorteados[i]){
+                        acertoDeNumeros += 1;
+                    }
+
+
+                }
+
+                if(i > 4){
+
+                    System.out.println("Informe o numero ESTRELA da " + (i+1) + "° posicao");
+                    int numero = ler.nextInt();
+
+                    while (numero < 1 || numero > 11 ){
+                        System.out.println("Valor invalido, informe dezenas entre 1 e 11. Digite novamente.");
+                        System.out.println("Informe o numero ESTRELA da " + (i+1) + "° posicao");
+
+                        numero = ler.nextInt();
+                    }
+
+                    if(numero == sorteados[i]){
+                        acertoDeEstrelas += 1;
+                    }
+
+                }
+
+            }
+
+
+            resultados.add(acertoDeNumeros + " NUMEROS " + acertoDeEstrelas + " ESTRELAS");
+
+            quantidadeDeJogos++;
+
+            if (quantidadeDeJogos == 5){
+                System.out.println("Voce ja jogou o maximo de vezes por hoje("+quantidadeDeJogos+"), volte a jogar amanhã");
+                resultados.forEach(resultado -> System.out.println(resultado));
+                break;
+
             }
 
         }
